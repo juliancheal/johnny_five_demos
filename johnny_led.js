c34501b2cf76c6e84b6ed77e5084a850d64bd151
Julian Cheal <julian.cheal@gmail.com>
2014-08-13 10:06:01 +0300

    Lea Pink

--- johnny_led.js
+++ johnny_led.js
@@ -3,6 +3,6 @@ var five = require("johnny-five");
 five.Board().on("ready", function() {
   var johnny_led = new five.Led.RGB([ 7, 8, 9 ]);
   
-  johnny_led.color("#ff006a");
+  johnny_led.color("#f06");
   
 });
